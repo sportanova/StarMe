ac97c28418c5d76081c7eda5f6204c7a7d42fa03
sportanova <sportano@gmail.com>
2014-12-06 02:57:39 -0800

    grabbing access code from github redirect

--- .dist-buildwrapper/src/Main.hs
+++ .dist-buildwrapper/src/Main.hs
@@ -1,9 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
 import Http.Github
+import Control.Monad.IO.Class(liftIO)
 
 main = scotty 3000 $ do
   get "/wat" $ do
     html "wat!"
   get "/who" $ do
-    json $ auth "1"
+    s <- liftIO stuff
+    json s
+  get "/auth_cb" $ do
+    code <- param "code"
+    r <- liftIO (doStuff code)
+    json r
+  get "/hello" $ do
+    name <- param "name"
+    text name
--- src/Http/Github.hs
+++ src/Http/Github.hs
@@ -3,11 +3,50 @@ module Http.Github where
 import Web.Scotty
 import Data.Aeson.Types
 import qualified Data.Text as T
+import Network.HTTP
+import Network.URI
 
-auth :: String -> Coord
-auth x = Coord {x = 1, y = 2}
+auth :: Coord
+auth = Coord {x = 1, y = 2}
 
+-- https://github.com/login/oauth/authorize?scope=user:email&client_id=99c89395ab6f347787e8
+
+stuff = do
+  rsp <- Network.HTTP.simpleHTTP (getRequest "http://www.haskell.org/")
+  r <- (getResponseBody rsp)
+  putStrLn r
+  return auth
+
+doStuff :: T.Text -> IO Print
+doStuff code = do
+  putStrLn $ T.unpack code
+  return Print {thing = T.unpack code}
+
+-- createAuthPostURL :: String -> String -> String -> String -> String
+-- createAuthPostURL clientId clientSecret code redirectURI = "https://github.com/login/oauth/access_token" ++ clientId 
+  
+createPostReq :: String -> T.Text -> Request String
+createPostReq url qParam = req where
+  uri = URI {uriPath = url}
+  rqMethod = POST
+  req = Request {rqURI = uri, rqMethod = rqMethod, rqHeaders = []}
+
+execPostReq :: String -> T.Text -> IO Print
+execPostReq url qParam = do
+  rsp <- simpleHTTP (createPostReq url qParam)
+  r <- (getResponseBody rsp)
+  putStrLn r
+  return Print {thing = r}
+
+  -- r <- fmap (take 100) (getResponseBody rsp)
 data Coord = Coord { x :: Double, y :: Double }
 
+data Print = Print {thing :: String}
+instance ToJSON Print where
+  toJSON (Print x) = object [T.pack "thing" .= x]
+
 instance ToJSON Coord where
-   toJSON (Coord x y) = object [T.pack "x" .= x, T.pack "y" .= y]
+  toJSON (Coord x y) = object [T.pack "x" .= x, T.pack "y" .= y]
+   
+--instance ToJSON IO a where
+  --toJSON (Coord x y) = object [T.pack "x" .= x, T.pack "y" .= y]
